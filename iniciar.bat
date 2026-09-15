bc1e97665d765437d2c6a3b3f3f5352b9aa3453b
Fernando Pereira Nilsen <hellcat.brasil@gmail.com>
2026-09-15 13:33:36 -0300

    Create iniciar.bat

---
+++ iniciar.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Entrando na pasta do projeto...
+cd /d "C:\Users\fernando.nilsen\Desktop\chamados-ti"
+
+echo Iniciando o servidor...
+npm start
+
+echo.
+echo =========================================================
+echo O servidor parou ou deu erro. Pressione algo para fechar.
+echo =========================================================
+pause
